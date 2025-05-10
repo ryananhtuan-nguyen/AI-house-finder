87cbaabeefba5c6708b833876f26cfbe0804b9a0
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2025-05-10 22:48:11 +1200

    fix: build

--- src/app/api/auth/trademe/check/route.ts
+++ src/app/api/auth/trademe/check/route.ts
@@ -2,8 +2,11 @@ import { NextResponse } from 'next/server'
 import { cookies } from 'next/headers'
 
 export async function GET() {
-  const accessToken = cookies().get('trademe_access_token')?.value
-  const accessTokenSecret = cookies().get('trademe_access_token_secret')?.value
+  const cookieStore = await cookies()
+  const accessToken = cookieStore.get('trademe_access_token')?.value
+  const accessTokenSecret = cookieStore.get(
+    'trademe_access_token_secret'
+  )?.value
 
   return NextResponse.json({
     authenticated: !!accessToken && !!accessTokenSecret,
